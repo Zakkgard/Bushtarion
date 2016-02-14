98ceb21a5f66a277142fc728e35eaf6a5aa78f81
Stayko <zakkgard@gmail.com>
2016-02-14 17:11:52 +0200

    implemeneted repository pattern

---
+++ Source/Data/Bushtarion.Data.Common/Models/IDeletableEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Bushtarion.Data.Common.Models
+{
+    using System;
+
+    public interface IDeletableEntity
+    {
+        bool IsDeleted { get; set; }
+
+        DateTime? DeletedOn { get; set; }
+    }
+}
--- Source/Data/Bushtarion.Data/ApplicationDbContext.cs
+++ Source/Data/Bushtarion.Data/ApplicationDbContext.cs
@@ -1,5 +1,10 @@
 ﻿namespace Bushtarion.Data
 {
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    using Bushtarion.Data.Common.Models;
     using Bushtarion.Data.Models;
 
     using Microsoft.AspNet.Identity.EntityFramework;
@@ -14,6 +19,29 @@
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        public override int SaveChanges()
+        {
+            this.ApplyAuditInfoRules();
+            return base.SaveChanges();
+        }
+
+        private void ApplyAuditInfoRules()
+        {
+            foreach (var entry in this.ChangeTracker.Entries().Where(e => e.Entity is IAuditInfo && ((e.State == EntityState.Modified) || (e.State == EntityState.Added))))
+            {
+                var entity = (IAuditInfo)entry.Entity;
+
+                if (entry.State == EntityState.Added && entity.CreatedOn == default(DateTime))
+                {
+                    entity.CreatedOn = DateTime.Now;
+                }
+                else
+                {
+                    entity.ModifiedOn = DateTime.Now;
+                }
+            }
         }
     }
 }
